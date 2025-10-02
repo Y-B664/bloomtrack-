dfd01ac71adad5f8f2c70ccc2885a534f453eb98
Y-B664 <yudyv-benavidesa@unilibre.edu.co>
2025-10-02 09:19:03 -0500

    Cambios de rutas

--- pruebaDatos.py
+++ pruebaDatos.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Ruta al archivo CSV descargado
-df = pd.read_csv("datasets/spotify/Dataset plantas.csv")
+df = pd.read_csv("datasets/Inventario plantas/Dataset plantas.csv")
 
 # Ver las primeras filas
 print(df.head())

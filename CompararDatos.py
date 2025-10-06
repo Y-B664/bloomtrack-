ffc2d82a60108008b5a851ec7886e790e2e1c59e
NicoleAbril07 <nicolec-abrilr@unilibre.edu.co>
2025-10-06 09:33:41 -0500

    Esquema parcial funciones

---
+++ CompararDatos.py
@@ -0,0 +1,21 @@
+
+
+
+def nombre_planta():
+    nombre=input("Ingresa el nombre de la planta: "))
+    return nombre
+
+def recoger_datos():
+    datos=
+    
+    entradas=tuples(datos)
+    return entradas 
+
+
+
+
+def comparar_datos():
+    comparar= recoger_datos()
+
+
+
